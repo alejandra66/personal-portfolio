1386a566afb76bfe8efc7bc703959204022dc83a
Alejandra Ramos <53943200+alejandra66@users.noreply.github.com>
2024-08-23 12:39:45 -0400

    create button component

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from "./Button";
 
 const Modal = ({ isOpen, onClose, imageUrl, modalTitle, modalContent, modalButton, link }) => {
     const navigate = useNavigate();
@@ -23,12 +24,11 @@ const Modal = ({ isOpen, onClose, imageUrl, modalTitle, modalContent, modalButto
                 <img src={imageUrl} alt={modalTitle} className="w-full h-auto object-cover rounded" />
                 <h2 className="text-xl font-bold mt-2">{modalTitle}</h2>
                 <p className="mt-2">{modalContent}</p>
-                <button
-                    type="button"
+                <Button
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                     onClick={handleButtonClick}
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                    {modalButton}
-                </button>
+                    label={modalButton}
+                />
             </div>
         </div>
     );
